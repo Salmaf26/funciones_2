5ce11fb64ebe6021cce3d1034f55c1983ddb1e9b
Salmaf26 <salmaf26c@gmail.com>
2023-06-15 20:06:25 -0600

    fundamentos

---
+++ clase2.js
@@ -0,0 +1,54 @@
+// para convertir  a mayusculas toUpperCase();
+let texto = 'Esto es un texto'
+texto=texto.toUpperCase();
+
+
+// para convertir  a minusculas toLowerCase();
+texto=texto.toLowerCase();
+
+console.log(texto);
+
+//longitud de un string
+let oracionLongitud = 'El oso come mucha miel ';
+oracionLongitud.charAt;
+
+console.log(oracionLongitud.length)
+
+//1-escribir un programa que reciba en una cadena y nos muestre
+//la longitud de la cadena
+let cadenaLongitud = 'Hola, como estas, esto es una cadena';
+cadenaLongitud.charAt;
+
+console.log(cadenaLongitud.length);
+//escribir un programa que devuelva la ultima parte de la oracion
+//el oso come mucha miel, usando split
+
+
+//escribir un programa que devuelva la cadena invertida
+//'prueba de porgramacion'
+
+//
+// let empleado = {
+// nombre:'Chico',
+// edad: 21,
+// salario: 35
+// };
+
+// console.log(`
+// Nombre de empleado: ${empleado.nombre}
+// Edad: ${empleado.edad}
+// Saldo total: ${empleado.total}
+// `);
+
+
+
+let vengador = {
+    nombre: 'Iron man',
+    edad: 'cuarenta años',
+    poderes: 'playboy'
+}
+
+vengador.nombre = 'Tony Stark';
+vengador.nombre = vengador.nombre.toUpperCase();
+
+console.log(vengador);
